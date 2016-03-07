5bb69802ef8c5aba1fe603efd31bc48fe7b38c6c
Дмитрий <vancor86>
2016-03-07 21:43:42 +0500

    Перенес лабу

---
+++ src/one.java
@@ -0,0 +1,12 @@
+/**
+ * Created by deL on 06.03.2016.
+ */
+  class one {
+    public static void main(String[] args){
+     int i;
+        for(i=2; i<=100; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
+
